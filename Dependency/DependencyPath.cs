93df3c0c21b797149b1b80efbc7f8dec8339847c
AkagiYuu <khanghuynh0245@gmail.com>
2022-05-08 19:15:03 +0700

    Initial commit

--- Dependency/DependencyPath.cs
+++ Dependency/DependencyPath.cs
@@ -7,7 +7,7 @@ namespace WinUI.Dependency
 {
     public static partial class DependencyPath
     {
-        public static string Installer = @"Dependency\Installer";
-        public static string Runtime = @"Dependency\Runtime";
+        public static string Installer = @"Path to Installer";
+        public static string Runtime = @"Path to WindowsAppRuntime";
     }
 }
--- Dependency/Installer/Install.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    using namespace std;
-    system("powershell -F InstallScript.ps1");
-}
