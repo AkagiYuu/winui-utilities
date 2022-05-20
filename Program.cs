b5599abb373a969f118f5e88e7543b58cb3d8d6e
AkagiYuu <khanghuynh0245@gmail.com>
2022-05-20 14:50:41 +0700

    Use CliFx

--- Program.cs
+++ Program.cs
@@ -1,24 +1,29 @@
 ﻿using WinUI.Extension;
 using WinUI.Utilities;
+using CliFx;
 
-switch (args.Length)
-{
-    case 0:
-        CommandlineHelper.PrintError("Missing arguments");
-        return;
-    case 1:
-        CommandlineHelper.PrintError("Missing object");
-        return;
-    case 2:
-        CommandlineHelper.PrintError("Missing target");
-        return;
-    case > 4:
-        CommandlineHelper.PrintError("Too many arguments");
-        return;
-}
+await new CliApplicationBuilder()
+            .AddCommandsFromThisAssembly()
+            .Build()
+            .RunAsync();
+// switch (args.Length)
+// {
+//     case 0:
+//         CommandlineHelper.PrintError("Missing arguments");
+//         return;
+//     case 1:
+//         CommandlineHelper.PrintError("Missing object");
+//         return;
+//     case 2:
+//         CommandlineHelper.PrintError("Missing target");
+//         return;
+//     case > 4:
+//         CommandlineHelper.PrintError("Too many arguments");
+//         return;
+// }
 
-var Action = args[0].ToTitleCase();
-var Object = args[1].ToTitleCase();
-var Target = args[2];
+// var Action = args[0].ToTitleCase();
+// var Object = args[1].ToTitleCase();
+// var Target = args[2];
 
-ClassUtility.InvokeStaticMethod(Action, Object, Target);
+// ClassUtility.InvokeStaticMethod(Action, Object, Target);
